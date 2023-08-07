32d0f432da09007fd87e79a90aa5c5051ce49e0f
Marcelo <marcelo2@ufpr.br>
2023-08-07 09:18:26 -0300

    pronto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner Valores = new Scanner(System.in);
+    
+        
+        System.out.println("Digite o número da conta:");
+        int NumConta = Valores.nextInt();
+
+        Valores.nextLine();
+
+        System.out.println("Digite o número da agência:");
+        String NumAg = Valores.nextLine();
+
+        System.out.println("Digite o seu nome:");
+        String Nome = Valores.nextLine();
+       
+        System.out.println("Digite o valor do depósito");
+        BigDecimal depósito = Valores.nextBigDecimal();
+
+        
+
+        System.out.println("Olá "+Nome+" sua conta foi criada, número: "+NumConta+" Agência: "+NumAg+" seu saldo é de:R$"+depósito);
     }
 }
